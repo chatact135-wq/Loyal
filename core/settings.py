4781d26fc19cadbdf1657400eeff798bc86a67bc
chatact135-wq <chatact135@gmail.com>
2026-08-01 22:51:46 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-restaurant-key-change-this')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://web-production-f557d.up.railway.app']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
